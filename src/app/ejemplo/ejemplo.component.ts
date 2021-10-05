b0ea803323df5c4ed75d9387edcb700fd97ae1fc
katamus <christian.catamuscay.h@gmail.com>
2021-10-05 14:34:28 -0500

    55. ejemplo uno bootstrap

--- src/app/ejemplo/ejemplo.component.ts
+++ src/app/ejemplo/ejemplo.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+class Alumno{
+
+  nombre:string = "";
+
+  apellido:string = "";
+
+  constructor(){
+    this.nombre = this.nombre;
+    this.apellido = this.apellido;
+  }
+
+}
+
 @Component({
   selector: 'app-ejemplo',
   templateUrl: './ejemplo.component.html',
@@ -7,9 +20,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EjemploComponent implements OnInit {
 
+  alumno: Alumno = new Alumno();
+  listaAlumno:Array<Alumno> = [];
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  agregarAlumno(){
+    this.listaAlumno.push({
+      apellido: this.alumno.apellido,
+      nombre:this.alumno.nombre
+    });
+    this.alumno.apellido = "";
+    this.alumno.nombre = "";
+  }
+  
 
 }
