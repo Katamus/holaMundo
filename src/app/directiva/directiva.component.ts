7eb55e56b320e702cf3a0ccbc244a421c8438e42
katamus <christian.catamuscay.h@gmail.com>
2021-10-04 21:49:40 -0500

    51. ngFor

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface articulo{
+  nombre:string;
+  cantidad:number;
+  fecha:Date;
+  empresa:string;
+}
+
 @Component({
   selector: 'app-directiva',
   templateUrl: './directiva.component.html',
@@ -8,6 +15,29 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent implements OnInit {
 
   cargando:boolean = true;
+  nombres:string[] = ["Lucas","Roberto","Patoja","Eduardo"];
+
+  articulos:Array<articulo> = [{
+   nombre:"arroz",
+   cantidad:100,
+   fecha:new Date("10/15/2021"),
+   empresa:"Roa"
+  },{
+    nombre:"Maiz",
+    cantidad:50,
+    fecha:new Date("10/16/2021"),
+    empresa:"Blanquita"
+  },{
+    nombre:"lentejas",
+    cantidad:25,
+    fecha:new Date("10/20/2021"),
+    empresa:"La tortuga"
+  },{
+    nombre:"Blanquiños",
+    cantidad:1000,
+    fecha:new Date("01/24/2022"),
+    empresa:"El gringo"
+  }]
 
   constructor() { }
 
